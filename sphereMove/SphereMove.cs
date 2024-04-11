ac3147400a598f2a09637f06458d11cbb643d12d
frede791 <frederik.anilmarkus@gmail.com>
2024-04-11 19:46:08 +0200

    functioning smooth motion
    Signed-off-by: frede791 <frederik.anilmarkus@gmail.com>

--- sphereMove/SphereMove.cs
+++ sphereMove/SphereMove.cs
@@ -7,6 +7,10 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading;
 
+using Unity.Jobs;
+using Unity.Burst;
+using Unity.Collections;
+
 
 public class SphereMove : MonoBehaviour
 {
@@ -14,43 +18,37 @@ public class SphereMove : MonoBehaviour
     CarData car = new CarData(); // the current car info
     float updateInterval = 1f / 3.7f; // x frequency of approximately 3.7Hz
     string url = "https://api.openf1.org/v1/location?session_key=9157&driver_number=81"; // An event at Monza
-    bool need_new_trajectory = true;
 
     List<float> listX = new List<float>(); // List for x positions
     List<float> listY = new List<float>(); // List for y positions
     List<DateTime> listTime = new List<DateTime>(); // List for times
 
-    List<float> interpolatedPosX = new List<float>(); // Store the interpolated positions
-    List<float> interpolatedPosY = new List<float>(); // Store the interpolated positions
-    List<DateTime> interpolatedPosTime = new List<DateTime>(); // Store the interpolated times
-    const int N = 7; // Do interpolation with 7 points ahead and 7 points behind
-    const int M = 15; // Interpolation points = N * M for each trajectory
+    const int threshold = 3; // # number of non-zero data points required to start
+
+    bool start_game = false;
     State currentState = State.Begin;
 
     DateTime current_tracking_time;
-    
+
 
     // Start is called before the first frame update
     void Start()
     {
         Debug.Log("State -> Begin");
         // this.gameObject.transform.localPosition = new Vector3(-320, -2052, -140); // Random pick of a initial point, can be deleted
-        StartCoroutine("GetCarData"); // Start the coroutine to retrieve data from openF1
-        StartCoroutine("DelayedUpdate");
+        StartCoroutine("FetchandCacheData"); // Start the coroutine to retrieve data from openF1
+        StartCoroutine("UpdateCarData");
+
     }
 
-    IEnumerator GetCarData()
+    IEnumerator FetchandCacheData()
     {
         while (true)
         {
-            float[] _x, _y;
-            float secStep;
-            int start_idx, end_idx;
-
             switch (currentState)
             {
                 case State.Begin:
-                    if (need_new_trajectory)
+                    if (!start_game)
                     {
                         using (UnityWebRequest www = UnityWebRequest.Get(url))
                         {
@@ -72,7 +70,7 @@ public class SphereMove : MonoBehaviour
                                 foreach (CarData data in carDataArray)
                                 {
                                     if (data.x == 0 && data.y == 0) { continue; }
-                                    if (count < N)
+                                    if (count < threshold)
                                     {
                                         listTime.Add(GetDateTime(data.date));
                                         listX.Add((float)(-data.x * 0.1));
@@ -84,112 +82,68 @@ public class SphereMove : MonoBehaviour
                                         break;
                                     }
                                 }
-                                if (count == N)
+                                if (count >= threshold)
                                 {
+                                    start_game = true;
                                     car.x = listX[0];
                                     car.y = listY[0];
                                     car.date = listTime[0].ToString("yyyy-MM-ddTHH:mm:ss.ffffff");
-                                    (_x, _y) = Cubic.InterpolateXY(listX.AsEnumerable().ToArray(), listY.AsEnumerable().ToArray(), (N - 1) * M);
-                                    secStep = (float)(listTime[N - 1] - listTime[0]).TotalSeconds / ((N - 1) * M - 1);
-                                    start_idx = 0;
-                                    end_idx = (int)Math.Ceiling((listTime[(N - 1) / 2 + 1] - listTime[0]).TotalSeconds / secStep - 1);
-                                    interpolatedPosX = _x.Skip(start_idx).Take(end_idx - start_idx + 1).ToList();
-                                    interpolatedPosY = _y.Skip(start_idx).Take(end_idx - start_idx + 1).ToList();
-                                    interpolatedPosTime.Add(listTime[0]);
-                                    for (int i = 1; i < interpolatedPosX.Count; i++)
-                                    {
-                                        interpolatedPosTime.Add(listTime[0].AddSeconds(secStep * (start_idx + i - 1)));
-                                    }
-                                    need_new_trajectory = false;
+                                    current_tracking_time = listTime[0];
+                                    Vector3 newPosition = new Vector3(car.x, car.y, this.gameObject.transform.localPosition.z);
+                                    this.gameObject.transform.localPosition = newPosition;
+                                    Debug.Log("Initial x=" + this.gameObject.transform.localPosition.x.ToString() + "y=" + this.gameObject.transform.localPosition.y.ToString());
+                                    currentState = State.Running;
+                                    Debug.Log("State -> Running");
+                                    yield return new WaitUntil( () => listX.Count < 3); // Wait for update interval before making the next request
                                 }
                                 else
                                 {
                                     listTime.Clear();
                                     listX.Clear();
                                     listY.Clear();
+                                    yield return null; // Wait for update interval second before making the next request
                                 }
                             }
                         }
                     }
-                    yield return new WaitForSeconds(1); // Wait for 1 second before making the next request
                     break;
                 case State.Running:
-                    if (need_new_trajectory)
+                    url = "https://api.openf1.org/v1/location?session_key=9157&driver_number=81&date>" + listTime[listTime.Count - 1].ToString("yyyy-MM-ddTHH:mm:ss.ffffff")
+                        + "&date<" + GetNextSecond(listTime[listTime.Count - 1].ToString("yyyy-MM-ddTHH:mm:ss.ffffff"), 2);
+                    Debug.Log("Try get url" + url); // Retrieve the next 2s car data
+                    using (UnityWebRequest www = UnityWebRequest.Get(url))
                     {
-                        url = "https://api.openf1.org/v1/location?session_key=9157&driver_number=81&date>" + listTime[listTime.Count - 1].ToString("yyyy-MM-ddTHH:mm:ss.ffffff")
-                            + "&date<" + GetNextSecond(listTime[listTime.Count - 1].ToString("yyyy-MM-ddTHH:mm:ss.ffffff"), 2);
-                        Debug.Log("Try get url" + url); // Retrieve the next 2s car data
-                        using (UnityWebRequest www = UnityWebRequest.Get(url))
-                        {
-                            yield return www.SendWebRequest();
+                        yield return www.SendWebRequest();
 
-                            if (www.result == UnityWebRequest.Result.ConnectionError || www.result == UnityWebRequest.Result.ProtocolError)
+                        if (www.result == UnityWebRequest.Result.ConnectionError || www.result == UnityWebRequest.Result.ProtocolError)
+                        {
+                            Debug.Log(www.error);
+                        }
+                        else
+                        {
+                            json = www.downloadHandler.text;
+                            if (json != "[]")
                             {
-                                Debug.Log(www.error);
+                                // Parse JSON array
+                                CarData[] carDataArray = JsonHelper.FromCustomJson<CarData>(json);
+
+                                foreach (CarData data in carDataArray)
+                                {
+                                    listTime.Add(GetDateTime(data.date));
+                                    listX.Add((float)(-data.x * 0.1));
+                                    listY.Add((float)(data.y * 0.1));
+                                }
                             }
                             else
                             {
-                                json = www.downloadHandler.text;
-                                if (json != "[]")
-                                {
-                                    // Parse JSON array
-                                    CarData[] carDataArray = JsonHelper.FromCustomJson<CarData>(json);
-                                    // Remove last car position and time
-                                    listX.RemoveAt(0);
-                                    listY.RemoveAt(0);
-                                    listTime.RemoveAt(0);
-
-                                    // Add next car position and time
-                                    listX.Add((float)(- carDataArray[0].x * 0.1));
-                                    listY.Add((float)(carDataArray[0].y * 0.1));
-                                    listTime.Add(GetDateTime(carDataArray[0].date));
-
-                                    // Interpolate the car position
-                                    (_x, _y) = Cubic.InterpolateXY(listX.AsEnumerable().ToArray(), listY.AsEnumerable().ToArray(), (N-1) * M);
-                                    // Find step size in interpolation in seconds
-                                    secStep = (float)(listTime[N - 1] - listTime[0]).TotalSeconds / ((N-1) * M - 1);
-                                    // Find first index after the middle of the array by time
-                                    start_idx = (int)Math.Floor((listTime[(N - 1) / 2] - listTime[0]).TotalSeconds / secStep + 1);
-                                    // Find last index before the middle of the array by time
-                                    end_idx = (int)Math.Ceiling((listTime[(N - 1) / 2 + 1] - listTime[0]).TotalSeconds / secStep - 1);
-
-                                    interpolatedPosX = _x.Skip(start_idx).Take(end_idx - start_idx + 1).ToList();
-                                    interpolatedPosY = _y.Skip(start_idx).Take(end_idx - start_idx + 1).ToList();
-                                    if (interpolatedPosX.Any(float.IsNaN) || interpolatedPosY.Any(float.IsNaN))
-                                    {
-                                        interpolatedPosX.Clear();
-                                        interpolatedPosY.Clear();
-                                        Debug.Log("NaN Trajectory, let car stay.");
-                                    }
-                                    else
-                                    {
-                                        // Add API position/time to the interpolated position/time at the beginning
-                                        interpolatedPosX.Insert(0, listX[(N - 1) / 2]);
-                                        interpolatedPosY.Insert(0, listY[(N - 1) / 2]);
-                                        interpolatedPosTime.Insert(0, listTime[(N - 1) / 2]);
-                                        for (int i = 1; i < interpolatedPosX.Count; i++)
-                                        {
-                                            interpolatedPosTime.Add(listTime[0].AddSeconds(secStep * (start_idx + i - 1)));
-                                        }
-                                        // New trajectory calculated. Do not calculate another one until interpolated positions have been used. 
-                                        need_new_trajectory = false;
-                                        // Update the car position
-                                        Debug.Log("New Trajectory generated");
-                                        yield return new WaitUntil(() => need_new_trajectory);
-                                    }
-                                }
-                                else
-                                {
-                                    Debug.Log("Empty measurement");
-                                    yield return new WaitForSeconds(updateInterval);
-                                }
+                                Debug.Log("Empty measurement");
                             }
                         }
                     }
-                    yield return new WaitForSeconds(updateInterval);
+                    yield return new WaitUntil( () => listX.Count < 3);
                     break;
                 default:
-                    yield return new WaitForSeconds(5);
+                    yield return null;
                     break;
             }
 
@@ -197,65 +151,42 @@ public class SphereMove : MonoBehaviour
     }
 
     bool isLerping = false; // Flag to track if lerping is in progress
-    // Update is called once per frame
-    IEnumerator DelayedUpdate()
+
+    IEnumerator UpdateCarData()
     {
         // Run through interpolatePosX elements and update when the respective time is reached
         while (true)
         {
-            switch (currentState)
+            if (start_game)
             {
-                case State.Begin:
-                    if (!need_new_trajectory)
-                    {
-                        // TODO, if at the beginning stage the car is still, will give a NaN traj and give an error, ut should be fine bcs it changes to the Running stage quickly
-                        current_tracking_time = listTime[0];
-                        for (int i = 0; i < interpolatedPosX.Count; i++)
-                        {
-                            car.x = interpolatedPosX[i];
-                            car.y = interpolatedPosY[i];
-                            car.date = interpolatedPosTime[i].ToString("yyyy-MM-ddTHH:mm:ss.ffffff");
-                            Vector3 newPosition = new Vector3(car.x, car.y, this.gameObject.transform.localPosition.z);
-                            isLerping = true;
-                            StartCoroutine(LerpToPosition(newPosition, (float)(interpolatedPosTime[i] - current_tracking_time).TotalSeconds));
-                            current_tracking_time = interpolatedPosTime[i];
-                            yield return new WaitUntil(() => !isLerping);
-                        }
-                        interpolatedPosTime.Clear();
-                        interpolatedPosX.Clear();
-                        interpolatedPosY.Clear();
-                        need_new_trajectory = true;
-                        currentState = State.Running;
-                        Debug.Log("State -> Running");
-                    }
-                    yield return new WaitUntil(() => !need_new_trajectory);
-                    break;
-                case State.Running:
-                    if (!need_new_trajectory)
-                    {
-                        for (int i = 0; i < interpolatedPosX.Count; i++)
-                        {
-                            car.x = interpolatedPosX[i];
-                            car.y = interpolatedPosY[i];
-                            car.date = interpolatedPosTime[i].ToString("yyyy-MM-ddTHH:mm:ss.ffffff");
-                            Vector3 newPosition = new Vector3(car.x, car.y, this.gameObject.transform.localPosition.z);
-                            isLerping = true;
-                            StartCoroutine(LerpToPosition(newPosition, (float)(interpolatedPosTime[i] - current_tracking_time).TotalSeconds));
-                            current_tracking_time = interpolatedPosTime[i];
-                            yield return new WaitUntil(() => !isLerping);
-                        }
-                        interpolatedPosTime.Clear();
-                        interpolatedPosX.Clear();
-                        interpolatedPosY.Clear();
-                        need_new_trajectory = true;
-                    }
-                    yield return new WaitUntil(() => !need_new_trajectory);
-                    break;
-                default:
+                Debug.Log("UpdateCarData while called.");
+                if (listX.Count >= 1)
+                {
+                    Debug.Log("UpdateCarData call Lerp.");
+                    car.x = listX[0];
+                    car.y = listY[0];
+                    car.date = listTime[0].ToString("yyyy-MM-ddTHH:mm:ss.ffffff");
+                    Vector3 newPosition = new Vector3(car.x, car.y, this.gameObject.transform.localPosition.z);
+                    isLerping = true;
+                    StartCoroutine(LerpToPosition(newPosition, (float)(listTime[0] - current_tracking_time).TotalSeconds));
+                    current_tracking_time = listTime[0];
+
+                    // Remove the first element in the lists 
+                    listX.RemoveAt(0);
+                    listY.RemoveAt(0);
+                    listTime.RemoveAt(0);
+                    yield return new WaitUntil(() => (!isLerping) && listX.Count >= 1);
+                }
+                else
+                {
+                    Debug.Log("WARNING: Not enough points in listX.");
                     yield return new WaitForSeconds(updateInterval);
-                    break;
+                }
             }
-            
+            else
+            {
+                yield return new WaitUntil(() => start_game);
+            }
         }
 
         // -------------- TODO ------------------
@@ -280,7 +211,7 @@ public class SphereMove : MonoBehaviour
     }
 
     // Return in format utcDateTime+durationSec
-    public static string GetNextSecond(string utcDateTime, int durationSec = 10)
+    public static string GetNextSecond(string utcDateTime, double durationSec = 10)
     {
         //Debug.Log("Input " + utcDateTime);
         // Parse the input string to DateTime object
