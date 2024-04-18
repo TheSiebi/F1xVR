b52daf3ffbd70cc722f0720e3cc6068490718bf1
frede791 <frederik.anilmarkus@gmail.com>
2024-04-18 17:49:43 +0200

    get rid of conflict stuff
    Signed-off-by: frede791 <frederik.anilmarkus@gmail.com>

--- movement/SmoothTracking.cs
+++ movement/SmoothTracking.cs
@@ -4,69 +4,37 @@ using System.Collections;
 using System;
 using System.Globalization;
 using System.Collections.Generic;
-<<<<<<< HEAD
-using System.Linq;
-using System.Threading;
-
-using Unity.Jobs;
-using Unity.Burst;
-using Unity.Collections;
-
-
-public class SphereMove : MonoBehaviour
-=======
 
 // Keep class name the same as file name
 public class SmoothTracking : MonoBehaviour
->>>>>>> origin/lerping
 {
     string json; // Variable to store received JSON data
     CarData car = new CarData(); // the current car info
 
-<<<<<<< HEAD
-    string url = "https://api.openf1.org/v1/location?session_key=9157&driver_number=81"; // An event at Monza
-=======
     string url; // URL to fetch from
     const string session_key = "9157";
     const string driver_number = "81";
->>>>>>> origin/lerping
 
     Queue<float> listX = new Queue<float>(); // List for x positions
     Queue<float> listY = new Queue<float>(); // List for y positions
     Queue<DateTime> listTime = new Queue<DateTime>(); // List for times
 
-<<<<<<< HEAD
-    const int threshold = 3; // # number of non-zero data points required to start
-
-    bool start_game = false;
-    State currentState = State.Begin;
-=======
     const int threshold = 15; // try to always keep at least 15 destinations in the listX
 
     bool start_game = false;
     bool is_fetching = false;
     bool isLerping = false; // Flag to track if lerping is in progress
->>>>>>> origin/lerping
 
     DateTime current_appending_time;
     DateTime current_tracking_time;
 
-<<<<<<< HEAD
-=======
     bool enableDebugLogs = false;
 
     Rigidbody rb; // Rigidbody component reference
->>>>>>> origin/lerping
 
     // Start is called before the first frame update
     void Start()
     {
-<<<<<<< HEAD
-        Debug.Log("State -> Begin");
-        // this.gameObject.transform.localPosition = new Vector3(-320, -2052, -140); // Random pick of a initial point, can be deleted
-        StartCoroutine("FetchandCacheData"); // Start the coroutine to retrieve data from openF1
-        StartCoroutine("UpdateCarData");
-=======
         rb = GetComponent<Rigidbody>(); // Get the Rigidbody component
         if (enableDebugLogs) Debug.Log("Start");
         StartCoroutine("WaitForGameBegin");
@@ -79,7 +47,7 @@ public class SmoothTracking : MonoBehaviour
             url = "https://api.openf1.org/v1/location?session_key=" + session_key + "&driver_number=" + driver_number; // An event at Monza
             using (UnityWebRequest www = UnityWebRequest.Get(url))
             {
-                yield return www.SendWebRequest();
+                yield return www.SendWebRequest();gi
 
                 if (www.result == UnityWebRequest.Result.ConnectionError || www.result == UnityWebRequest.Result.ProtocolError)
                 {
@@ -131,130 +99,10 @@ public class SmoothTracking : MonoBehaviour
 
             }
         }
->>>>>>> origin/lerping
     }
 
     IEnumerator FetchandCacheData()
     {
-<<<<<<< HEAD
-
-        while (true)
-        {
-            switch (currentState)
-            {
-                case State.Begin:
-                    if (!start_game)
-                    {
-                        using (UnityWebRequest www = UnityWebRequest.Get(url))
-                        {
-                            yield return www.SendWebRequest();
-
-                            if (www.result == UnityWebRequest.Result.ConnectionError || www.result == UnityWebRequest.Result.ProtocolError)
-                            {
-                                Debug.Log(www.error);
-                            }
-                            else
-                            {
-                                json = www.downloadHandler.text;
-                                // Parse JSON array as an array of CarData {[x:,y:,z:,date:,...], [x:,y:,z:,date:,...], [x:,y:,z:,date:,...]}
-                                CarData[] carDataArray = JsonHelper.FromCustomJson<CarData>(json);
-
-                                // Check if any entry has 'x' or 'y' not zero, meaning car moves
-                                // This is for real-time game, if the car has not moved, the x y will all be 0
-                                foreach (CarData data in carDataArray)
-                                {
-                                    if (data.x == 0 && data.y == 0) { continue; }
-                                    if (listY.Count < threshold + 1)
-                                    {
-                                        listTime.Enqueue(GetDateTime(data.date));
-                                        listX.Enqueue((float)(-data.x * 0.1));
-                                        listY.Enqueue((float)(data.y * 0.1));
-                                        current_appending_time = GetDateTime(data.date);
-                                    }
-                                    else // Get N points
-                                    {
-                                        break;
-                                    }
-                                }
-                                if (listY.Count >= threshold + 1)
-                                {
-                                    start_game = true;
-                                    car.x = listX.Dequeue();
-                                    car.y = listY.Dequeue();
-                                    current_tracking_time = listTime.Dequeue();
-                                    car.date = current_tracking_time.ToString("yyyy-MM-ddTHH:mm:ss.ffffff");
-
-                                    Vector3 newPosition = new Vector3(car.x, car.y, this.gameObject.transform.localPosition.z);
-                                    this.gameObject.transform.localPosition = newPosition;
-                                    Debug.Log("Initial x=" + this.gameObject.transform.localPosition.x.ToString() + "y=" + this.gameObject.transform.localPosition.y.ToString());
-                                    currentState = State.Running;
-                                    Debug.Log("State -> Running");
-                                    yield return new WaitUntil(() => listX.Count < threshold); // Wait for update interval before making the next request
-                                }
-                                else
-                                {
-                                    listTime.Clear();
-                                    listX.Clear();
-                                    listY.Clear();
-                                    yield return null; // Wait for update interval second before making the next request
-                                }
-                            }
-                        }
-                    }
-                    break;
-                case State.Running:
-                    url = "https://api.openf1.org/v1/location?session_key=9157&driver_number=81&date>" + current_appending_time.ToString("yyyy-MM-ddTHH:mm:ss.ffffff")
-                        + "&date<" + GetNextSecond(current_appending_time.ToString("yyyy-MM-ddTHH:mm:ss.ffffff"), 20);
-                    Debug.Log("Try get url" + url); // Retrieve the next 2s car data
-                    using (UnityWebRequest www = UnityWebRequest.Get(url))
-                    {
-                        yield return www.SendWebRequest();
-
-                        if (www.result == UnityWebRequest.Result.ConnectionError || www.result == UnityWebRequest.Result.ProtocolError)
-                        {
-                            Debug.Log(www.error);
-                        }
-                        else
-                        {
-                            json = www.downloadHandler.text;
-                            if (json != "[]")
-                            {
-                                // Parse JSON array
-                                CarData[] carDataArray = JsonHelper.FromCustomJson<CarData>(json);
-
-                                foreach (CarData data in carDataArray)
-                                {
-                                    listTime.Enqueue(GetDateTime(data.date));
-                                    listX.Enqueue((float)(-data.x * 0.1));
-                                    listY.Enqueue((float)(data.y * 0.1));
-                                    current_appending_time = GetDateTime(data.date);
-                                    yield return null;
-                                }
-                            }
-                            else
-                            {
-                                Debug.Log("Empty measurement");
-                            }
-                        }
-                    }
-                    yield return new WaitUntil(() => listX.Count < threshold);
-                    break;
-                default:
-                    yield return null;
-                    break;
-            }
-
-        }
-    }
-
-    bool isLerping = false; // Flag to track if lerping is in progress
-
-    IEnumerator UpdateCarData()
-    {
-        yield return new WaitUntil(() => start_game && listX.Count >= 2);
-        // Run through interpolatePosX elements and update when the respective time is reached
-        while (true)
-=======
         url = "https://api.openf1.org/v1/location?session_key=9157&driver_number=81&date>" + current_appending_time.ToString("yyyy-MM-ddTHH:mm:ss.ffffff")
             + "&date<" + GetNextSecond(current_appending_time.ToString("yyyy-MM-ddTHH:mm:ss.ffffff"), 2);
         if (enableDebugLogs) Debug.Log("Try get url" + url); // Retrieve the next 2s car data
@@ -306,7 +154,6 @@ public class SmoothTracking : MonoBehaviour
             if (enableDebugLogs) Debug.LogWarning("Update frame no lerping destination!");
         } 
         else if ( (!isLerping) && listX.Count >= 1 )
->>>>>>> origin/lerping
         {
             car.x = listX.Dequeue();
             car.y = listY.Dequeue();
@@ -314,19 +161,6 @@ public class SmoothTracking : MonoBehaviour
             car.date = destination_time.ToString("yyyy-MM-ddTHH:mm:ss.ffffff");
             Vector3 newPosition = new Vector3(car.x, car.y, this.gameObject.transform.localPosition.z);
             isLerping = true;
-<<<<<<< HEAD
-            StartCoroutine(LerpToPosition(newPosition, (float)(destination_time - current_tracking_time).TotalSeconds));
-            current_tracking_time = destination_time;
-
-            yield return new WaitUntil(() => (!isLerping) && listX.Count >= 2);
-        }
-
-        // -------------- TODO ------------------
-        // StopCoroutine("GetCarData") when race finished
-        // Either stop it here in update() or stop it in the IEnumerator
-    }
-
-=======
             // Start the coroutine to lerp to the first destination in list
             StartCoroutine(LerpToPosition(newPosition, (float)(destination_time - current_tracking_time).TotalSeconds));
             current_tracking_time = destination_time;
@@ -339,12 +173,10 @@ public class SmoothTracking : MonoBehaviour
     }
 
 
->>>>>>> origin/lerping
     IEnumerator LerpToPosition(Vector3 targetPosition, float duration)
     {
         Vector3 startPosition = this.gameObject.transform.localPosition;
         float startTime = Time.time;
-<<<<<<< HEAD
         Vector3 up = new Vector3(0,0,1);
         Vector3 direction;
         while ((Time.time - startTime) < duration)
@@ -364,42 +196,20 @@ public class SmoothTracking : MonoBehaviour
         }
         // Ensure the final position is exactly the target position
         this.gameObject.transform.localPosition = targetPosition;
-=======
-        while ((Time.time - startTime) < duration)
-        {
-            rb.MovePosition(Vector3.Lerp(startPosition, targetPosition, (Time.time - startTime) / duration));
-            if (enableDebugLogs) Debug.Log("Lerp update x=" + this.gameObject.transform.localPosition.x.ToString() + "y=" + this.gameObject.transform.localPosition.y.ToString());
-            yield return null;
-        }
-        // Ensure the final position is exactly the target position
-        rb.MovePosition(targetPosition);
->>>>>>> origin/lerping
         isLerping = false;
     }
 
     // Return in format utcDateTime+durationSec
     public static string GetNextSecond(string utcDateTime, double durationSec = 10)
     {
-<<<<<<< HEAD
-        //Debug.Log("Input " + utcDateTime);
-=======
->>>>>>> origin/lerping
         // Parse the input string to DateTime object
         if (DateTime.TryParseExact(utcDateTime, "yyyy-MM-ddTHH:mm:ss.ffffff",
                                     CultureInfo.InvariantCulture,
                                     DateTimeStyles.None,
                                     out DateTime dt))
         {
-<<<<<<< HEAD
-            // Add one second to the parsed DateTime object
-            dt = dt.AddSeconds(durationSec);
-
-            // Format the DateTime object to ISO 8601 format
-            //Debug.Log("Output " + dt.ToString("yyyy-MM-ddTHH:mm:ss.ffffff"));
-=======
             dt = dt.AddSeconds(durationSec);
             // Format the DateTime object to ISO 8601 format
->>>>>>> origin/lerping
             return dt.ToString("yyyy-MM-ddTHH:mm:ss.ffffff");
         }
         else
@@ -409,11 +219,6 @@ public class SmoothTracking : MonoBehaviour
         }
     }
 
-<<<<<<< HEAD
-    public static DateTime GetDateTime(string utcDateTime)
-    {
-        //Debug.Log("Input " + utcDateTime);
-=======
     public DateTime GetDateTime(string utcDateTime)
     {
         // Have no idea why there's a fking '2023-09-03T13:48:36' without microsec in Monza date data
@@ -423,7 +228,6 @@ public class SmoothTracking : MonoBehaviour
             utcDateTime += ".000000";
         }
 
->>>>>>> origin/lerping
         // Parse the input string to DateTime object
         if (DateTime.TryParseExact(utcDateTime, "yyyy-MM-ddTHH:mm:ss.ffffff",
                                     CultureInfo.InvariantCulture,
@@ -434,26 +238,11 @@ public class SmoothTracking : MonoBehaviour
         }
         else
         {
-<<<<<<< HEAD
-            Debug.Log("Cannot get DateTime!");
-=======
             if (enableDebugLogs) Debug.LogWarning("Cannot get DateTime!");
->>>>>>> origin/lerping
             return DateTime.MinValue;
         }
     }
 
-<<<<<<< HEAD
-    public enum State
-    {
-        Begin,
-        Running,
-        Ending,
-        Stopped
-    }
-
-=======
->>>>>>> origin/lerping
     // Define a class to hold car data structure
     [System.Serializable]
     public class CarData
@@ -482,8 +271,4 @@ public class SmoothTracking : MonoBehaviour
             public T[] array;
         }
     }
-<<<<<<< HEAD
 }
-=======
-}
->>>>>>> origin/lerping
